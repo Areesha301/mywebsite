fefa14f27a42e2aa1a58517fd974202e105baa14
asp <asp@aptech.com>
2024-04-25 20:30:52 +0500

    productgit remote add origin https://github.com/Areesha301/my-website.git git branch -M main git push -u origin main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'firstapp';
+  title = 'Ishraq Ahmed';
+  msg="batchcode 2303c1"
+  age=19;
 }
